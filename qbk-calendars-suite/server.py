070a68f56b87cca988fcfd9cef33630784e4bccf
shwaz49999 <joshschwartztv@gmail.com>
2026-09-11 17:30:50 -0400

    Treat Volley Royale as private event

--- qbk-calendars-suite/server.py
+++ qbk-calendars-suite/server.py
@@ -979,6 +979,9 @@ class DashClient:
         if "battle royale" in haystack:
             return "private_event"
 
+        if "volley royale" in haystack:
+            return "private_event"
+
         if "birthday" in haystack:
             return "private_event"
 
